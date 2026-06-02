5c6a07c2521e25f27af080ea5b183991c34748b6
GabrielFraga <gabrielbff2006@gmail.com>
2026-06-02 20:59:34 -0300

    chore: packages and maven configuration set up

---
+++ aspmanager/src/main/java/br/com/ucsal/aspmanager/AspmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ucsal.aspmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspmanagerApplication.class, args);
+	}
+
+}
---
+++ aspmanager/src/test/java/br/com/ucsal/aspmanager/AspmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ucsal.aspmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
