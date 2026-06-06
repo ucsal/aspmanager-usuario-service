cc7855218a74d09ef0ccd1c95116d2034942d3e2
SilvaOrdep <pedro.o.silva975@gmail.com>
2026-06-06 00:05:39 -0300

    feat: endpoint de auth interno e docs OpenAPI
    - Cria GET /api/v1/usuarios/email para uso exclusivo do ms-auth via Feign
    - Adiciona path de auth interno as excecoes do GatewayHeaderFilter
    - Define @OpenAPIDefinition para o titulo no Swagger UI

--- src/main/java/br/com/ucsal/aspmanager/AspmanagerApplication.java
+++ src/main/java/br/com/ucsal/aspmanager/AspmanagerApplication.java
@@ -1,9 +1,12 @@
 package br.com.ucsal.aspmanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "ASPManager API - Usuários", version = "1.0", description = "Microserviço de Gestão de Usuários e Professores"))
 public class AspmanagerApplication {
 
 	public static void main(String[] args) {
